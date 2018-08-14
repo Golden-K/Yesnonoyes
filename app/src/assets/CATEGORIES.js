2b3a5046eccd8716116eab7d29354d93b8df9c22
Erik <erikaanestad@gmail.com>
2018-08-14 12:26:41 -0700

    added /app

---
+++ app/src/assets/CATEGORIES.js
@@ -0,0 +1,355 @@
+export { GLOBAL_CATEGORIES };
+const GLOBAL_CATEGORIES = [
+  {
+    'alias': 'bagels',
+    'category': 'Bagels'
+  },
+  {
+    'alias': 'baguettes',
+    'category': 'Baguettes'
+  },
+  {
+    'alias': 'bakeries',
+    'category': 'Bakeries'
+  },
+  {
+    'alias': 'bbq',
+    'category': 'BBQ'
+  },
+  {
+    'alias': 'bistros',
+    'category': 'Bistros'
+  },
+  {
+    'alias': 'breakfast_brunch',
+    'category': 'Brunch'
+  },
+  {
+    'alias': 'breweries',
+    'category': 'Breweries'
+  },
+  {
+    'alias': 'british',
+    'category': 'British'
+  },
+  {
+    'alias': 'buffets',
+    'category': 'Buffets'
+  },
+  {
+    'alias': 'burgers',
+    'category': 'Burgers'
+  },
+  {
+    'alias': 'cafes',
+    'category': 'Cafes'
+  },
+  {
+    'alias': 'cafeteria',
+    'category': 'Cafeteria'
+  },
+  {
+    'alias': 'cajun',
+    'category': 'Cajun'
+  },
+  {
+    'alias': 'canteen',
+    'category': 'Canteen'
+  },
+  {
+    'alias': 'caribbean',
+    'category': 'Caribbean'
+  },
+  {
+    'alias': 'chicken_wings',
+    'category': 'Chicken Wings'
+  },
+  {
+    'alias': 'chinese',
+    'category': 'Chinese'
+  },
+  {
+    'alias': 'churros',
+    'category': 'Churros'
+  },
+  {
+    'alias': 'creperies',
+    'category': 'Creperies'
+  },
+  {
+    'alias': 'cuban',
+    'category': 'Cuban'
+  },
+  {
+    'alias': 'delicatessen',
+    'category': 'Delicatessen'
+  },
+  {
+    'alias': 'delis',
+    'category': 'Delis'
+  },
+  {
+    'alias': 'dimsum',
+    'category': 'Dimsum'
+  },
+  {
+    'alias': 'diners',
+    'category': 'Diners'
+  },
+  {
+    'alias': 'dumplings',
+    'category': 'Dumplings'
+  },
+  {
+    'alias': 'empanadas',
+    'category': 'Empanadas'
+  },
+  {
+    'alias': 'ethiopian',
+    'category': 'Ethiopian'
+  },
+  {
+    'alias': 'falafel',
+    'category': 'Falafel'
+  },
+  {
+    'alias': 'fishnchips',
+    'category': 'Fish \'n Chips'
+  },
+  {
+    'alias': 'flatbread',
+    'category': 'Flatbread'
+  },
+  {
+    'alias': 'fondue',
+    'category': 'Fondue'
+  },
+  {
+    'alias': 'food_court',
+    'category': 'Food Court'
+  },
+  {
+    'alias': 'foodstands',
+    'category': 'Food Stands'
+  },
+  {
+    'alias': 'foodtrucks',
+    'category': 'Food Trucks'
+  },
+  {
+    'alias': 'french',
+    'category': 'French'
+  },
+  {
+    'alias': 'gamemeat',
+    'category': 'Game Meat'
+  },
+  {
+    'alias': 'gastropubs',
+    'category': 'Gastropubs'
+  },
+  {
+    'alias': 'german',
+    'category': 'German'
+  },
+  {
+    'alias': 'giblets',
+    'category': 'Giblets'
+  },
+  {
+    'alias': 'gourmet',
+    'category': 'Gourmet'
+  },
+  {
+    'alias': 'greek',
+    'category': 'Greek'
+  },
+  {
+    'alias': 'halal',
+    'category': 'Halal'
+  },
+  {
+    'alias': 'hawaiian',
+    'category': 'Hawaiian'
+  },
+  {
+    'alias': 'homemadefood',
+    'category': 'Homemade Food'
+  },
+  {
+    'alias': 'hotdogs',
+    'category': 'Hotdogs'
+  },
+  {
+    'alias': 'hungarian',
+    'category': 'Hungarian'
+  },
+  {
+    'alias': 'italian',
+    'category': 'Italian'
+  },
+  {
+    'alias': 'japanese',
+    'category': 'Japanese'
+  },
+  {
+    'alias': 'kebab',
+    'category': 'Kebab'
+  },
+  {
+    'alias': 'korean',
+    'category': 'Korean'
+  },
+  {
+    'alias': 'meatballs',
+    'category': 'Meatballs'
+  },
+  {
+    'alias': 'mediterranean',
+    'category': 'Mediterranean'
+  },
+  {
+    'alias': 'mexican',
+    'category': 'Mexican'
+  },
+  {
+    'alias': 'mideastern',
+    'category': 'Middle Eastern'
+  },
+  {
+    'alias': 'nightfood',
+    'category': 'Night Food'
+  },
+  {
+    'alias': 'noodles',
+    'category': 'Noodles'
+  },
+  {
+    'alias': 'pita',
+    'category': 'Pita'
+  },
+  {
+    'alias': 'pizza',
+    'category': 'Pizza'
+  },
+  {
+    'alias': 'poke',
+    'category': 'Poke'
+  },
+  {
+    'alias': 'popuprestaurants',
+    'category': 'Popup Restaurants'
+  },
+  {
+    'alias': 'poutineries',
+    'category': 'Poutineries'
+  },
+  {
+    'alias': 'pretzels',
+    'category': 'Pretzels'
+  },
+  {
+    'alias': 'pubfood',
+    'category': 'Pub Food'
+  },
+  {
+    'alias': 'ramen',
+    'category': 'Ramen'
+  },
+  {
+    'alias': 'raw_food',
+    'category': 'Raw Food'
+  },
+  {
+    'alias': 'sandwiches',
+    'category': 'Sandwiches'
+  },
+  {
+    'alias': 'scandinavian',
+    'category': 'Scandinavian'
+  },
+  {
+    'alias': 'schnitzel',
+    'category': 'Schnitzel'
+  },
+  {
+    'alias': 'seafood',
+    'category': 'Seafood'
+  },
+  {
+    'alias': 'smokehouse',
+    'category': 'Smokehouse'
+  },
+  {
+    'alias': 'soulfood',
+    'category': 'Soulfood'
+  },
+  {
+    'alias': 'soup',
+    'category': 'Soup'
+  },
+  {
+    'alias': 'southern',
+    'category': 'Southern'
+  },
+  {
+    'alias': 'spanish',
+    'category': 'Spanish'
+  },
+  {
+    'alias': 'steak',
+    'category': 'Steak'
+  },
+  {
+    'alias': 'streetvendors',
+    'category': 'Street Vendors'
+  },
+  {
+    'alias': 'sushi',
+    'category': 'Sushi'
+  },
+  {
+    'alias': 'szechuan',
+    'category': 'Szechuan'
+  },
+  {
+    'alias': 'tacos',
+    'category': 'Tacos'
+  },
+  {
+    'alias': 'taiwanese',
+    'category': 'Taiwanese'
+  },
+  {
+    'alias': 'tamales',
+    'category': 'Tamales'
+  },
+  {
+    'alias': 'tex-mex',
+    'category': 'Tex-Mex'
+  },
+  {
+    'alias': 'thai',
+    'category': 'Thai'
+  },
+  {
+    'alias': 'tortillas',
+    'category': 'Tortillas'
+  },
+  {
+    'alias': 'turkish',
+    'category': 'Turkish'
+  },
+  {
+    'alias': 'vietnamese',
+    'category': 'Vietnamese'
+  },
+  {
+    'alias': 'waffles',
+    'category': 'Waffles'
+  },
+  {
+    'alias': 'wraps',
+    'category': 'Wraps'
+  }
+];
